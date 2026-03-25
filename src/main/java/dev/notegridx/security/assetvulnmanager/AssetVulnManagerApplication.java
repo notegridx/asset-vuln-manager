0b673d215c9c141bb025bcdfe80cf95e904b520c
notegridx <251776887+notegridx@users.noreply.github.com>
2026-03-25 14:32:42 +0900

    fix(schedule): enable Spring scheduling to ensure CVE Delta Update and Generate Alerts jobs run

--- src/main/java/dev/notegridx/security/assetvulnmanager/AssetVulnManagerApplication.java
+++ src/main/java/dev/notegridx/security/assetvulnmanager/AssetVulnManagerApplication.java
@@ -2,8 +2,11 @@ package dev.notegridx.security.assetvulnmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class AssetVulnManagerApplication {
 
 	public static void main(String[] args) {
